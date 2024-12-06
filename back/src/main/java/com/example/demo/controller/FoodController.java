2c077c4088fc01ff2e3312ee1c605050bf19b2c0
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2024-12-06 16:39:45 +0100

    feat: add controller to upload images in server

--- back/src/main/java/com/example/demo/controller/FoodController.java
+++ back/src/main/java/com/example/demo/controller/FoodController.java
@@ -5,10 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Food;
+import com.example.demo.payload.FoodRequest;
 import com.example.demo.service.FoodService;
 
 import io.swagger.annotations.Api;
@@ -28,7 +30,7 @@ public class FoodController {
     @GetMapping
     public ResponseEntity getFood() {
 
-        return ResponseEntity.status(200).body("nothing");
+        return ResponseEntity.status(200).body(foodService.getFoods());
     }
 
     @Operation(summary = "Get all Foods from your fridge", description = "You can get all the foods you have saved in your fridge")
@@ -43,6 +45,14 @@ public class FoodController {
     public ResponseEntity addFood(@PathVariable("bareCode") String bareCode) {
 
         foodService.save(new Food(bareCode));
+        return ResponseEntity.status(200).body("confirmed");
+    }
+
+    @Operation(summary = "Add new food with bareCode", description = "Give a bareCode of the food to add it")
+    @PutMapping("/{bareCode}")
+    public ResponseEntity updateFood(@PathVariable("bareCode") String bareCode, FoodRequest foodRequest) {
+
+        Food food = foodService.getFood(bareCode);
         return ResponseEntity.status(200).body("confirmed");
     }
 }
--- back/src/main/java/com/example/demo/controller/ImageController.java
+++ back/src/main/java/com/example/demo/controller/ImageController.java
@@ -2,38 +2,81 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.service.FoodService;
+
 import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/image")
 @SecurityRequirement(name = "Authorization")
-@Api(tags = "User", description = "Endpoint")
+@Api(tags = "Image", description = "Endpoint")
 public class ImageController {
 
-    @Value("${image.storage.path}") 
+    @Value("${image.storage.path}")
     private String imageStoragePath;
-    
+
+    private FoodService foodService;
+
     @GetMapping("/{filename}")
     public ResponseEntity<Resource> getImage(@PathVariable String filename) {
         try {
             Resource file = new org.springframework.core.io.FileSystemResource(
-                Paths.get(imageStoragePath).resolve(filename).toFile()
-            );
+                    Paths.get(imageStoragePath).resolve(filename).toFile());
 
             return ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_TYPE, "image/jpeg")
                     .body(file);
         } catch (Exception e) {
             return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping(path = "/upload/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity uploadImage(@RequestParam(value = "file") MultipartFile file, @PathParam("id") String id) {
+        // return "(ResponseEntity) ResponseEntity.ok()";
+        try {
+            // Vérification du fichier
+            if (file.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Aucun fichier sélectionné");
+            }
+
+            //Generate new fileName
+            String originalFilename = file.getOriginalFilename();
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String uniqueFilename = UUID.randomUUID() + extension;
+
+            //Save the image
+            Path filePath = Paths.get(imageStoragePath, "/" + uniqueFilename);
+            System.out.println(filePath);
+            Files.createDirectories(filePath.getParent());
+            file.transferTo(filePath);
+
+            return ResponseEntity.ok("Image saved");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erreur lors de l'upload de l'image");
         }
     }
 }
