827db7a43c73bc0936195a98a331208af611ba07
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2024-12-07 16:44:26 +0100

    feat: add angular project and CI/CD pipeline

--- front/angular/src/app/app.component.spec.ts
+++ front/angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ecoFridge_front_web'`, () => {
+  it(`should have as title 'angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ecoFridge_front_web');
+    expect(app.title).toEqual('angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ecoFridge_front_web');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular');
   });
 });
--- front/angular/src/app/app.component.ts
+++ front/angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ecoFridge_front_web';
+  title = 'angular';
 }
