3ce8862c971ab7c8c41ffb875192aa5db7c166ae
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-08 22:03:48 +0100

    test: add test for myfridge view

--- front/angular/src/app/user-myfridge/user-myfridge.component.spec.ts
+++ front/angular/src/app/user-myfridge/user-myfridge.component.spec.ts
@@ -1,9 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserMyfridgeComponent } from './user-myfridge.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FoodService } from '../services/food.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Food } from '../responses/FoodInterface';
+import { of } from 'rxjs';
 
 describe('UserMyfridgeComponent', () => {
   let component: UserMyfridgeComponent;
@@ -14,9 +15,9 @@ describe('UserMyfridgeComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [UserMyfridgeComponent, NavbarComponent],
       providers: [FoodService],
-      imports:[HttpClientTestingModule]
+      imports: [HttpClientTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(UserMyfridgeComponent);
     component = fixture.componentInstance;
@@ -27,4 +28,23 @@ describe('UserMyfridgeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should fetch data using FoodService', () => {
+    const food: Food = { 'idFood': 0, 'image': 'default.png', 'bareCode': "001", 'safeImageURL': '' }
+    let FoodArray = [
+      food
+    ]
+
+    spyOn(foodService, "getFood").and.returnValue(of(FoodArray));
+    component.ngOnInit();
+
+    expect(component.food).toBe(FoodArray);
+    expect(foodService.getFood).toHaveBeenCalled();
+  });
+
+  it('should have a div for fridge', () => {
+    const element = fixture.nativeElement.querySelector('#fridge');
+    expect(element).toBeTruthy();
+  });
+
 });
