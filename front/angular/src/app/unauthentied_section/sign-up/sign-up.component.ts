6e65dc438bf8f91b88f66023c430f35d0e2210e6
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-10 16:50:40 +0100

    feat: signup view ready, navigation between home and signup views

--- front/angular/src/app/unauthentied_section/sign-up/sign-up.component.ts
+++ front/angular/src/app/unauthentied_section/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  formLogin!: FormGroup
+  signInError = "";
 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authservice: AuthService,) {
+  }
+
+  ngOnInit(): void {
+
+    this.formLogin = this.fb.group({
+      mail: this.fb.control(""),
+      password: this.fb.control("")
+    })
+  }
+
+  handleLogin() {}
 }
