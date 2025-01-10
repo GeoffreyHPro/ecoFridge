3660130fae131e3653d95d8e63c2d2a0d9a3e5d6
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-10 16:56:57 +0100

    test: add injection of AuthService and import ReactiveFormsModule

--- front/angular/src/app/unauthentied_section/sign-up/sign-up.component.spec.ts
+++ front/angular/src/app/unauthentied_section/sign-up/sign-up.component.spec.ts
@@ -1,19 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SignUpComponent } from './sign-up.component';
+import { AuthService } from '../../services/auth.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
   let fixture: ComponentFixture<SignUpComponent>;
+  let authservice: AuthService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SignUpComponent]
+      declarations: [SignUpComponent],
+      providers: [AuthService],
+      imports: [HttpClientTestingModule, ReactiveFormsModule ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(SignUpComponent);
     component = fixture.componentInstance;
+    authservice = TestBed.inject(AuthService)
     fixture.detectChanges();
   });
 
