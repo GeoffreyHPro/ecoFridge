b8cf3d9c612f225841699a66ac3060ab13c0e3f9
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-11 10:41:36 +0100

    feat: signup works to create a new account

--- front/angular/src/app/unauthentied_section/sign-up/sign-up.component.ts
+++ front/angular/src/app/unauthentied_section/sign-up/sign-up.component.ts
@@ -26,5 +26,21 @@ export class SignUpComponent {
     })
   }
 
-  handleLogin() {}
+  handleSignup() {
+    let mail = this.formSignup.value.mail;
+    let password = this.formSignup.value.password;
+
+    this.authservice.signUp(mail, password).subscribe(
+      response => {
+        let resp = response;
+        console.log(resp)
+        if (response.status == 200) {
+          this.router.navigateByUrl("home")
+        }
+      },
+      error => {
+        console.log("error")
+      }
+    )
+  }
 }
