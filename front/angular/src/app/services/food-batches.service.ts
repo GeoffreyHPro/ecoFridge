9a4f95e101024dc72db61690fb75880ce1806321
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-14 10:28:37 +0100

    feat: get foodbatches in user-home, will modify to get expiration foodbatches only

--- front/angular/src/app/services/food-batches.service.ts
+++ front/angular/src/app/services/food-batches.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { DataFoodBatches } from '../responses/DataFoodBatchesInterface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,12 @@ export class FoodBatchesService {
 
   constructor(private http: HttpClient) { }
 
-  getFoodBatches(): Observable<any> {
+  getFoodBatches(): Observable<DataFoodBatches> {
     const token = localStorage.getItem("Token")!
     const headers = new HttpHeaders({
       Authorization: `Bearer ${token}` 
     });
 
-    return this.http.get("http://localhost:8080/foodbatch", { headers });
+    return this.http.get<DataFoodBatches>("http://localhost:8080/foodbatch", { headers });
   }
 }
--- front/angular/src/app/user-home/user-home.component.ts
+++ front/angular/src/app/user-home/user-home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FoodBatchesService } from '../services/food-batches.service';
+import { FoodService } from '../services/food.service';
+import { FoodBatch } from '../responses/FoodBatchInterface';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-user-home',
@@ -8,15 +11,29 @@ import { FoodBatchesService } from '../services/food-batches.service';
 })
 export class UserHomeComponent {
   signInError: string | undefined;
+  foodbatches: FoodBatch[] = [];
+
   constructor(
-    private foodBatchesService: FoodBatchesService){
+    private foodBatchesService: FoodBatchesService,
+    private foodService: FoodService,
+    private sanitizer: DomSanitizer
+  ) {
   }
 
   ngOnInit(): void {
     this.foodBatchesService.getFoodBatches().subscribe(
       response => {
         let resp = response;
-        console.log(resp)
+        this.foodbatches = resp.data;
+        console.log(resp.data)
+
+        this.foodbatches.forEach((foodBatch) => {
+          this.foodService.getImage(foodBatch.food.image).subscribe((blob) => {
+            const objectUrl = URL.createObjectURL(blob);
+            foodBatch.safeImageURL = this.sanitizer.bypassSecurityTrustUrl(objectUrl);
+          })
+        })
+
       },
       error => {
         console.error('Erreur lors de la récupération des données :', error);
