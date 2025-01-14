8a33f81e2ad049f52c67002b7738661948b9a190
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-14 18:55:32 +0100

    feat: click on button lougout delete localStorage token/role and redirect to home

--- front/angular/src/app/authentified_section/navbar/navbar.component.ts
+++ front/angular/src/app/authentified_section/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private router: Router) {
 
+  }
+
+  logout() {
+    localStorage.removeItem("Token");
+    localStorage.removeItem("Role");
+    this.router.navigateByUrl("home")
+  }
 }
