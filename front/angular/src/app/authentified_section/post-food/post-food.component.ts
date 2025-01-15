a5f6d723215fbfb9538d2087990f0396d178bad9
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-15 22:40:34 +0100

    chore: add environment file for baseUrl, and add form to add foodbatch

--- front/angular/src/app/authentified_section/post-food/post-food.component.ts
+++ front/angular/src/app/authentified_section/post-food/post-food.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { FoodService } from '../../services/food.service';
+import { FoodBatchesService } from '../../services/food-batches.service';
 
 @Component({
   selector: 'app-post-food',
@@ -9,11 +10,13 @@ import { FoodService } from '../../services/food.service';
 })
 export class PostFoodComponent {
   formAddFood!: FormGroup;
+  formAddFoodBatch!: FormGroup;
   messageError: string = "";
 
   constructor(
     private fb: FormBuilder,
-    private foodService: FoodService) {
+    private foodService: FoodService,
+    private foodBatchService: FoodBatchesService) {
   }
 
   ngOnInit(): void {
@@ -22,18 +25,34 @@ export class PostFoodComponent {
       foodName: this.fb.control(""),
       foodDescription: this.fb.control("")
     })
+
+    this.formAddFoodBatch = this.fb.group({
+      foodBarcode: this.fb.control(""),
+      expirationDate: this.fb.control(""),
+      foodQuantity: this.fb.control(0)
+    })
   }
 
   handleAddFood() {
     this.foodService.addFood(this.formAddFood.value.foodBarcode).subscribe(
       response => {
-        console.log(response);
       }, error => {
         if (error.status == 409) {
           this.messageError = "The food with this bar code is already created";
         }
       }
     )
+  }
+
+  handleAddFoodBatch() {
+    this.foodBatchService.addFoodBatch("0001",
+      this.formAddFoodBatch.value.foodQuantity, this.formAddFoodBatch.value.expirationDate + "T20:05:10").subscribe(
+        response => {
+          console.log(response)
+        }, error => {
+          console.log(error)
+        }
+      )
   }
 
 }
--- front/angular/src/app/services/auth.service.ts
+++ front/angular/src/app/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  private baseUrl = environment.apiUrl;
   constructor(private http: HttpClient) { }
 
 
@@ -17,7 +18,7 @@ export class AuthService {
       'Content-Type': 'application/json'
     });
 
-    return this.http.post("http://localhost:8080/auth/signIn", bodyUser, { headers, observe: "response" });
+    return this.http.post(`${this.baseUrl}/auth/signIn`, bodyUser, { headers, observe: "response" });
   }
 
   signUp(username: string, password: string): Observable<any> {
@@ -27,6 +28,6 @@ export class AuthService {
       'Content-Type': 'application/json'
     });
 
-    return this.http.post("http://localhost:8080/auth/signUp", bodyUser, { headers, observe: "response" });
+    return this.http.post(`${this.baseUrl}/auth/signUp`, bodyUser, { headers, observe: "response" });
   }
 }
