35c83338629b647b68c5738336267f236eba8dff
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-02-11 23:37:50 +0100

    test: add tests for carroussels

--- front/angular/src/app/authentified_section/user-home/food-carroussel/food-carroussel.component.spec.ts
+++ front/angular/src/app/authentified_section/user-home/food-carroussel/food-carroussel.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FoodCarrousselComponent } from './food-carroussel.component';
+import { By } from '@angular/platform-browser';
 
 describe('FoodCarrousselComponent', () => {
   let component: FoodCarrousselComponent;
@@ -10,14 +11,44 @@ describe('FoodCarrousselComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [FoodCarrousselComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(FoodCarrousselComponent);
     component = fixture.componentInstance;
+    component.listFoodbatches = [];
+    component.typeListFoodBatch = "expired";
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have h1 with red color', () => {
+    const h1 = fixture.debugElement.query(By.css('h1'));
+    const h1Element = h1.nativeElement;
+
+    expect(h1).toBeTruthy();
+    expect(h1Element.textContent).toContain('Food');
+    expect(getComputedStyle(h1Element).color).toBe('rgb(165, 27, 27)');
+  });
+
+  it('should have p balise without element in list', () => {
+    const p = fixture.debugElement.query(By.css('p'));
+    const pElement = p.nativeElement;
+
+    expect(p).toBeTruthy();
+    expect(pElement.textContent).toContain('Congratulations ! None food is');
+  });
+
+  it('should have h1 with orange color', () => {
+    component.typeListFoodBatch = "soon expired";
+    fixture.detectChanges();
+    const h1 = fixture.debugElement.query(By.css('h1'));
+    const h1Element = h1.nativeElement;
+
+    expect(h1).toBeTruthy();
+    expect(h1Element.textContent).toContain('Food');
+    expect(getComputedStyle(h1Element).color).toBe('rgb(255, 165, 0)');
   });
 });
