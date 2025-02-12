a5b77ea2dafcac6a4d738744794a9c8219e001c1
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-02-12 14:25:54 +0100

    feat: add form add food in modify view, add background image

--- front/angular/src/app/authentified_section/post-food/post-food.component.ts
+++ front/angular/src/app/authentified_section/post-food/post-food.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { FoodService } from '../../services/food.service';
-import { MatDialog } from '@angular/material/dialog';
-import { PopupMessageComponent } from '../../utils/popup-message/popup-message.component';
 
 @Component({
   selector: 'app-post-food',
@@ -10,38 +6,8 @@ import { PopupMessageComponent } from '../../utils/popup-message/popup-message.c
   styleUrl: './post-food.component.css'
 })
 export class PostFoodComponent {
-  formAddFood!: FormGroup;
-  addFoodErrorMessage: string = "";
 
-  constructor(
-    private fb: FormBuilder,
-    private foodService: FoodService,
-    private dialog: MatDialog) {
+  constructor() {
   }
 
-  handleAddFood() {
-    this.foodService.addFood(this.formAddFood.value.foodBarcode).subscribe(
-      response => {
-        this.showMessage("Add food", "The food given is correctly added");
-        this.addFoodErrorMessage = "";
-      }, error => {
-        if (error.status == 409) {
-          this.addFoodErrorMessage = "The food with this bar code is already created";
-        }
-      }
-    )
-  }
-
-  showMessage(title: string, message: string) {
-    const dialogRef = this.dialog.open(PopupMessageComponent, {
-      data: {
-        title: title,
-        message: message
-      }
-    });
-
-    setTimeout(() => {
-      dialogRef.close();
-    }, 3000);
-  }
 }
