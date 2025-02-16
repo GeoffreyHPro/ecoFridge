66afbf819c06b79d8e1b4f1c32a5821789adce03
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-02-16 14:34:03 +0100

    chore: add ImageService and update ImageController

--- back/src/main/java/com/example/demo/controller/ImageController.java
+++ back/src/main/java/com/example/demo/controller/ImageController.java
@@ -15,15 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.service.FoodService;
+import com.example.demo.service.ImageService;
 
 import org.springframework.core.io.Resource;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
 
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -40,6 +38,9 @@ public class ImageController {
 
     @Autowired
     private FoodService foodService;
+
+    @Autowired
+    private ImageService imageService;
 
     @GetMapping("/{filename}")
     public ResponseEntity<Resource> getImage(@PathVariable String filename) {
@@ -63,35 +64,20 @@ public class ImageController {
     @PostMapping(path = "/upload/{bareCode}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> uploadImage(@RequestParam(value = "file") MultipartFile file,
             @PathParam("bareCode") String bareCode) {
+
+        if (file.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No one file has been uploaded");
+        }
         try {
-            // Vérification du fichier
-            if (file.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Aucun fichier sélectionné");
-            }
-
-            // Generate new fileName
-            String originalFilename = file.getOriginalFilename();
-            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
-            String uniqueFilename = UUID.randomUUID() + extension;
-
-            // Save the image
-            Path filePath = Paths.get(imageStoragePath, "/" + uniqueFilename);
-            System.out.println(filePath);
-            Files.createDirectories(filePath.getParent());
-            file.transferTo(filePath);
-
-            // Update pathfile in database
-            foodService.updateFoodImage(bareCode, uniqueFilename);
-
+            imageService.saveFile(file, bareCode);
             return ResponseEntity.ok("Image saved");
-
         } catch (IOException e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erreur lors de l'upload de l'image");
         }
     }
 
-    boolean isFile(String pathFile){
+    boolean isFile(String pathFile) {
         File file = new File(pathFile);
         return file.exists() && file.isFile();
     }
